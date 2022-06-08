eb23bcfdc077fda42db881a8776d224bbf7fb9fb
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-06-08 20:16:07 +0700

    Commit 2
    Add Function of sum,supl and mult with two fractions or fraction and integer

--- main.cpp
+++ main.cpp
@@ -5,16 +5,28 @@ using namespace std;
 
 class Fraction
 {
-	int num;
-	int den;
+	int num{};
+	int den{};
+	int nod{};
 	
 public:
 	Fraction(int numP, int denP)
-		: num{ numP }, den{ denP }{}
+		: num{ numP }, den{ denP }{
+		}
 	Fraction() : Fraction(1, 1) {}
 	~Fraction() {}	
-	void print(){	
-		cout << num << " / " << den << endl;
+	void print(){
+		if (num < 0) {
+			cout << "  "<<num * -1 <<endl;
+			cout << "- -" << endl;
+			cout << "  " << den << endl;
+		}
+		else {
+			cout << "  " << num  << endl;
+			cout << "  -" << endl;
+			cout << "  " << den << endl;
+		}
+			
 	}
 	void setNum(int num) {
 		this->num = num;
@@ -42,18 +54,48 @@ public:
 	}
 	Fraction subt(Fraction a, Fraction b) {
 		Fraction c;
-
+		if (a.den == b.den) {
+			c.num = a.num - b.num;
+			c.den = a.den;
+		}
+		else {
+			c.den = a.den * b.den;
+			c.num = (a.num * b.den) - (b.num * a.den);
+		}
+		if (c.num == 0) {
+			c.den = 0;
+			c.num = 0;
+		}
+		return c;
 	}
-
+	Fraction subt(Fraction a, int b) {
+		Fraction c;
+		c.den = a.den;
+		c.num = a.num - (b * a.den);
+		return c;
+	}
+	Fraction mult(Fraction a, Fraction b) {
+		Fraction c;
+		c.num = a.num * b.num;
+		c.den = a.den * b.den;
+		return c;
+	}
+	Fraction mult(Fraction a, int b) {
+		Fraction c;
+		c.num = a.num * b;
+		c.den = a.den;
+		return c;
+	}
 };
 int main()
 {
 	
-	Fraction a(1, 4);
-	Fraction b(2,6);
+	Fraction a(1, 5);
+	a.print();
+	Fraction b(2,5);
 	Fraction c;
-	int d = 5;
-	c =c.sum(a, d);
+	int d = 4;
+	c = c.subt(a,b);
 	c.print();
 	
 	return 0;
